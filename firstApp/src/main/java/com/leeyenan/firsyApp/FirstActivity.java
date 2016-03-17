afea2a90a9935ea5a1cf35a50f3c6aa3fea71d7f
lyn <490070237@qq.com>
2016-03-17 20:16:00 +0800

    add FirstActivity

---
+++ firstApp/src/main/java/com/leeyenan/firsyApp/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.leeyenan.firsyApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by LeeYeNan on 2016/3/17 0017.
+ */
+public class FirstActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
